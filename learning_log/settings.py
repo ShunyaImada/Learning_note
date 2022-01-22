ab92cb42f41fe453b5e850040d77b26fb0200453
shunya <imada@example.com>
2022-01-22 23:40:19 +0900

    Herokuにデプロイする準備が完了

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,7 +137,7 @@ LOGIN_URL = 'users:login'
 
 # Heroku no setting
 import django_heroku
-django_heroku,settings(locals())
+django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
